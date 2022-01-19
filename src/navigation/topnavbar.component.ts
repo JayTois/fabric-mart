61088d277831cf1dbe1e5114b14767f1f6dfd5a4
JayTois <jtlizmi6@gmail.com>
2022-01-18 20:08:05 -0500

    Added a top navbar component

---
+++ src/navigation/topnavbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'fm-topnavbar',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+    })
+export class TopNavBarComponent{
+
+}
